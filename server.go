e5d2c495ef515015eeb593f25ebb7ba7a65b8043
adrian <adrianpinoalcalde@gmail.com>
2016-12-10 22:32:10 +0100

    asyncronous posts

--- server.go
+++ server.go
@@ -3,7 +3,7 @@ package main
 // imports with the underscore are just drivers, without it you have full access
 import (
 	"fmt"
-//	"time"
+	"time"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"database/sql"
@@ -12,32 +12,34 @@ import (
 
 type Info struct {
     Name     string `json:"name"`
-    Time string `json:"time"`
+    Datetime string `json:"datetime"`
 }
 
+type params struct {
+    context *gin.Context
+    statement *sql.Stmt
+    row Info
+}
 
 func main() {
-	db, cn_err := sql.Open("mysql", "root:mysqlroot@(127.0.0.1:3306)/test_db")
-	if cn_err != nil {
-		panic(cn_err.Error())
-	}
+    fmt.Println("Starting server")
+    db, cn_err := sql.Open("mysql", "root:mysqlroot@(127.0.0.1:3306)/test_db")
+    if cn_err != nil {
+            panic(cn_err.Error())
+    }
     stmtIns, err := db.Prepare("INSERT INTO test (`name`, `time`) VALUES( ?, ? )") // ? = placeholder
     if err != nil {
         panic(err.Error()) // proper error handling instead of panic in your app
     }
-    defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
+    defer stmtIns.Close()
 
     router := gin.Default()
 
     router.POST("/post", func(c *gin.Context) {
         var json Info
-//		t0 := time.Now()
-//		fmt.Println(t0)
-		// This basically means that the json could be parsed
         if c.BindJSON(&json) == nil {
-			fmt.Println(json.Name, json.Time)
-			c.JSON(http.StatusOK, gin.H{"status": "all good"})
-			_, err = stmtIns.Exec(json.Name, json.Time) 
+                par := params{context: c, statement: stmtIns, row: json}
+                go InsertDb(par)
         }
     })
 
@@ -45,4 +47,20 @@ func main() {
     router.Run(":8080")
 }
 
-func insert_db
+//func InsertDb(stmtIns *sql.Stmt, json Info) {
+func InsertDb(par params) {
+            rw := par.row
+            context := par.context
+            stmt:= par.statement
+            if len(rw.Datetime) == 0 {
+                rw.Datetime = time.Now().Format("2006-01-02 15:04:05")
+            }
+            _, ins_err := stmt.Exec(rw.Name, rw.Datetime)
+
+            if ins_err != nil {
+                fmt.Println(ins_err.Error())
+            } else {
+                context.Status(http.StatusOK)
+
+            }
+}
